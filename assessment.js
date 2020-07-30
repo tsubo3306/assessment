a9b9f90145ebf7fa2cda0ec03b7c1c680cf3acef
tsubo3306 <63445071+tsubo3306@users.noreply.github.com>
2020-07-30 21:23:57 +0900

    あなたのいいところ診断のファイル

---
+++ assessment.js
@@ -0,0 +1,86 @@
+'use strict';
+const userNameInput = document.getElementById('user-name');
+const assessmentButton = document.getElementById('assessment');
+const resultDivided = document.getElementById('result_area');
+const tweetDivided = document.getElementById('tweet_area');
+/**
+ * 
+ * @param {} element 
+ */
+function removeAllChildren(element){
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    };
+}
+
+userNameInput.onkeydown = event => {
+    if (event.key === 'Enter') {
+        assessmentButton.onclick();
+    }
+}
+assessmentButton.onclick = () => {
+    const userName = userNameInput.value;
+    if (userName.length === 0){
+        return;
+    };
+    removeAllChildren(resultDivided);
+    const header = document.createElement('h3');
+    header.innerText = 'assessment result';
+    resultDivided.appendChild(header);
+
+    const result = assessment(userName);
+    const paragraph = document.createElement('p');
+    paragraph.innerText = result;
+    resultDivided.appendChild(paragraph);
+
+    removeAllChildren(tweetDivided);
+    const a = document.createElement('a');
+    const href = "https://twitter.com/intent/tweet?button_hashtag=あなたのいいところ&ref_src=twsrc%5Etfw";
+    a.setAttribute('href', href);
+    a.className = "twitter-hashtag-button";
+    a.setAttribute('data-text', result);
+    a.innerText = 'Tweet #あなたのいいところ';
+    tweetDivided.appendChild(a);
+
+    const script = document.createElement('script');
+    script.setAttribute('src', "https://platform.twitter.com/widgets.js");
+    tweetDivided.appendChild(script);
+
+};
+
+const answers = [
+'{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
+'{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
+'{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。',
+'{userName}のいいところは厳しさです。{userName}の厳しさがものごとをいつも成功に導きます。',
+'{userName}のいいところは知識です。博識な{userName}を多くの人が頼りにしています。',
+'{userName}のいいところはユニークさです。{userName}だけのその特徴が皆を楽しくさせます。',
+'{userName}のいいところは用心深さです。{userName}の洞察に、多くの人が助けられます。',
+'{userName}のいいところは見た目です。内側から溢れ出る{userName}の良さに皆が気を惹かれます。',
+'{userName}のいいところは決断力です。{userName}がする決断にいつも助けられる人がいます。',
+'{userName}のいいところは思いやりです。{userName}に気をかけてもらった多くの人が感謝しています。',
+'{userName}のいいところは感受性です。{userName}が感じたことに皆が共感し、わかりあうことができます。',
+'{userName}のいいところは節度です。強引すぎない{userName}の考えに皆が感謝しています。',
+'{userName}のいいところは好奇心です。新しいことに向かっていく{userName}の心構えが多くの人に魅力的に映ります。',
+'{userName}のいいところは気配りです。{userName}の配慮が多くの人を救っています。',
+'{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。',
+'{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。',
+];
+/**
+ * 
+ * @param {string} userName ユーザの名前
+ * @return {string} 診断結果
+ */
+function assessment(userName){
+    let number = 0;
+    for (let i = 0; i < userName.length; i++) {
+        number += userName.charCodeAt(i);
+    }
+    let idx = number % answers.length;
+    return answers[idx].replace(/\{userName\}/g, userName);
+}
+
+console.assert(
+    assessment('太郎') === '太郎のいいところは決断力です。太郎がする決断にいつも助けられる人がいます。',
+    '','error'
+);
